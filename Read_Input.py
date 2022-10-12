29aa3a2efb23b8d01e52ed416dedc947eaec86ae
sukanta <sukanta6@gmail.com>
2022-10-12 17:48:40 +0530

    update

--- Read_Input.py
+++ Read_Input.py
@@ -9,7 +9,7 @@ df = pd.DataFrame({"A":[5, 3, 6, 4],
                    "D":[5, 4, 2, 8]})
 
 
-# Cumulative sum
+# Cumulative sum of data
 df1 = df.cumsum(axis = 0, skipna = True)
 print(df1.dtypes)
 
@@ -18,7 +18,7 @@ lst = [['AAA',2019,15],['BBB',2019,16],['BBB',2020,22],['AAA',2020,-20],['CCC',2
 df = pd.DataFrame(lst,columns = ['name','year','val'])
 
 
-df.loc[df['name'].isin(df.loc[df['val'].lt(0), 'name']), 'val'] = 1
+#df.loc[df['name'].isin(df.loc[df['val'].lt(0), 'name']), 'val'] = 1
 
 df.loc[df['val'].lt(0).groupby(df['name']).transform('any'), 'val'] = 1
 
