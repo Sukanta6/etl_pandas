d8dde41f70d6ecb0b7add3543e1751457bb39c89
sukanta <sukanta6@gmail.com>
2023-03-01 20:01:51 +0530

    Interview question

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-from Setup_file import test
+my_list=[1,5,3,6,8,2,-1]
+new_list = []
 
-p = test()
-#print(p)
+while my_list:
+    min = my_list[0]  
+    for x in my_list: 
+        if x < min:
+            min = x
+    new_list.append(min)
+    my_list.remove(min)    
+
+print(new_list)
